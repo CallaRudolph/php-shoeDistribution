9e606c6e8da41ee7cd554fbcfbcf36f9d05ba2d8
Calla Rudolph <callarudolph@gmail.com>
2017-07-21 12:03:02 -0700

    create route to add_stores to a shoe brand

--- app/app.php
+++ app/app.php
@@ -65,7 +65,7 @@
         $store = Store::find($_POST['store_id']);
         $shoe = Shoe::find($_POST['shoe_id']);
         $store->addShoe($shoe);
-        return $app['twig']->render('student.html.twig', array('store' => $store, 'stores' => Store::getAll(), 'shoes' => $store->getShoes(), 'all_shoes' => Shoe::getAll()));
+        return $app['twig']->render('store.html.twig', array('store' => $store, 'stores' => Store::getAll(), 'shoes' => $store->getShoes(), 'all_shoes' => Shoe::getAll()));
     });
 
     //////////////////////////////////////
@@ -90,6 +90,13 @@
     $app->get("/shoe/{id}", function($id) use ($app) {
         $shoe = Shoe::find($id);
         return $app['twig']->render('shoe.html.twig', array('shoe' => $shoe, 'stores' => $shoe->getStores(), 'all_stores' => Store::getAll()));
+    });
+
+    $app->post("/add_stores", function() use ($app) {
+        $shoe = Shoe::find($_POST['shoe_id']);
+        $store = Store::find($_POST['store_id']);
+        $shoe->addStore($store);
+        return $app['twig']->render('shoe.html.twig', array('shoe' => $shoe, 'shoes' => Shoe::getAll(), 'stores' => $shoe->getStores(), 'all_stores' => Store::getAll()));
     });
 
     return $app;
